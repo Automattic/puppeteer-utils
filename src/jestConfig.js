5d501f0e32745e3287cdea5c5c581b4a28e89ae7
Brent Sessions <brent.sessions@automattic.com>
2020-01-09 10:13:52 -0700

    Removing babel

--- src/jestConfig.js
+++ src/jestConfig.js
@@ -2,13 +2,13 @@ const currentDir = __dirname;
 
 export const jestConfig = {
     preset: 'jest-puppeteer',
+    transformIgnorePatterns: [
+      'node_modules/(?!(puppeteer-utils)/)'
+    ],
     setupFiles: [`${currentDir}/setup.js`],
     setupFilesAfterEnv: [
         `${currentDir}/setup-env.js`,
         `${currentDir}/jest.test.failure.js`,
         'expect-puppeteer'
-    ],
-    transformIgnorePatterns: [
-        'node_modules/(?!(puppeteer-utils)/)'
     ]
 };
