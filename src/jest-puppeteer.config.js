4f6b5f756c02310f91a0c8048448e6b0e3bb080a
Brent Sessions <brent.sessions@automattic.com>
2020-01-17 09:40:57 -0700

    Removing since it is getting overwritten later

--- src/jest-puppeteer.config.js
+++ src/jest-puppeteer.config.js
@@ -26,10 +26,6 @@ module.exports = {
             '--window-size=1920,1080',
             '--user-agent=puppeteer-debug',
         ],
-        defaultViewport: {
-            width: 1280,
-            height: 800,
-        },
         // Required for the logged out and logged in tests so they don't share app state/token.
         browserContext: 'incognito',
     },
